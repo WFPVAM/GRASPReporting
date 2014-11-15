23b95436ed2fbe9f8a62b37cb823d868b6e49009
MarcoGiorgi <marco.giorgi@brainsen.com>
2014-11-15 09:34:30 +0100

    Update to Sql scripts for upgrade112
    Fixed problems on some scripts for the DB upgrade to v.1.1.2.

--- SQLDbScripts/UpdateTo112/10_UserFilters.sql
+++ SQLDbScripts/UpdateTo112/10_UserFilters.sql
@@ -1,21 +1,24 @@
 USE [GRASP]
 GO
 
-/****** Object:  Table [dbo].[UserToFormResponses]    Script Date: 11/07/2014 18:07:27 ******/
+/****** Object:  Table [dbo].[UserFilters]    Script Date: 11/14/2014 12:06:28 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[UserToFormResponses](
-	[UserToFormResponsesID] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[UserFilters](
+	[UserFilterID] [int] IDENTITY(1,1) NOT NULL,
 	[userID] [int] NOT NULL,
-	[formResponseID] [numeric](19, 0) NOT NULL,
+	[UserFilterString] [nvarchar](max) NULL,
 	[formID] [numeric](19, 0) NOT NULL,
- CONSTRAINT [PK_UserToFormResponses] PRIMARY KEY CLUSTERED 
+	[UserFilterCreateDate] [datetime] NOT NULL,
+	[UserFilterIsEnabled] [int] NOT NULL,
+	[UserFilterDescription] [nvarchar](max) NULL,
+ CONSTRAINT [PK_UserFilters] PRIMARY KEY CLUSTERED 
 (
-	[UserToFormResponsesID] ASC
+	[UserFilterID] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
---
+++ SQLDbScripts/UpdateTo112/23_AddRecordToRoles.sql
@@ -0,0 +1,2 @@
+INSERT INTO GRASP_OLD.dbo.Roles (description) VALUES ('Reviewer');
+INSERT INTO GRASP_OLD.dbo.Roles (description) VALUES ('Reader');
