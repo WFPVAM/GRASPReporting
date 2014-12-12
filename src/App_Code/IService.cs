54ae57a3d86b97ff55df6525894c88fb2a79eed4
ematramu <ematramu@gmail.com>
2014-12-12 16:51:19 +0100

    Add Method 'CreateView'

--- src/App_Code/IService.cs
+++ src/App_Code/IService.cs
@@ -7,6 +7,11 @@ using System.ServiceModel.Web;
 public interface IService
 {
     [OperationContract][WebInvoke(Method = "GET",ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped,
-    UriTemplate = "Surveys/{formId=null}/{formResponseId=null}")]
-    Message Surveys(string formId, string formResponseId);
+    UriTemplate = "Surveys/{formId=null}/{columnNames=null}/{formResponseId=null}")]
+    Message Surveys(string formId, string columnNames, string formResponseId);
+
+    [OperationContract]
+    [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped,
+    UriTemplate = "CreateView/{viewName}/{tableName}/{columnNames}/{whereClause=null}/{orderbyClause=null}")]
+    Message CreateView(string viewName, string tableName, string columnNames, string whereClause, string orderbyClause);
 }
--- src/App_Code/Service.cs
+++ src/App_Code/Service.cs
@@ -1,8 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Reflection;
@@ -11,57 +12,267 @@ using System.Runtime.Serialization;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Web;
 using System.Text;
-using System.Web;
 
 public class Service : IService
 {
-    private List<object> Surveys()
+    #region "Interface Methods"
+    Message IService.Surveys(string formId, string columnNames, string formResponseId)
     {
-        List<Form> listForm = null;
         List<object> listReturn = null;
-        List<object> listReturnTmp = null;
+        List<string> listColumnNames = null;
 
-        using (GRASPEntities db = new GRASPEntities())
+        if (string.IsNullOrEmpty(formId))
         {
-            var item = (from f in db.Form
-                        select f);
-
-            if (item != null)
-                listForm = (List<Form>)item.ToList();
+            throw new WebFaultException<string>("The ID Parent Form is missing.", HttpStatusCode.BadRequest);
         }
 
-        if (listForm == null || listForm.Count == 0)
-            RESTException("No form in DB", HttpStatusCode.NotFound);
-
-        foreach (Form f in listForm)
+        if (string.IsNullOrEmpty(columnNames))
         {
-            listReturnTmp = getListFormResponse(f.id.ToString(), null);
-
-            if (listReturn == null) listReturn = new List<object>();
-
-            if (listReturnTmp != null && listReturnTmp.Count > 0) listReturn.AddRange(listReturnTmp);
+            throw new WebFaultException<string>("Column names are missing.", HttpStatusCode.BadRequest);
         }
 
-        return listReturn;
+        listColumnNames = columnNames.Split(new char[] { ',' }).ToList();
+
+        foreach (string s in listColumnNames)
+            if (string.IsNullOrEmpty(s.Trim())) listColumnNames.Remove(s);
+
+        if (listColumnNames.Count == 0)
+        {
+            throw new WebFaultException<string>("Column names are missing.", HttpStatusCode.BadRequest);
+        }
+
+        listReturn = getListFormResponse(formId, listColumnNames, formResponseId);
+
+        if (listReturn == null || listReturn.Count == 0)
+        {
+            throw new WebFaultException<string>("No data", HttpStatusCode.NotFound);
+        }
+
+        return WebOperationContext.Current.CreateTextResponse(JsonConvert.SerializeObject(listReturn),
+            "application/json; charset=utf-8", Encoding.UTF8);
     }
 
-    private List<object> getListFormResponse(string idParentForm, string formResponseId)
+    Message IService.CreateView(string viewName, string tableName, string columnNames, string whereClause, string orderbyClause)
+    {
+        string connectionString = getConnectionString("GRASP_MemberShip");
+        string sqltestExistingView = "select count(*) FROM sys.all_objects where name = '" + viewName + "'";
+        string sqlCreateView = "CREATE VIEW " + viewName + " AS ";
+
+        try
+        {
+            //check if there's an object with the same name
+            int count = (int)ExecuteScalar(connectionString, CommandType.Text, sqltestExistingView, null, null);
+            if (count > 0)
+            {
+                throw new WebFaultException<string>("The view '" + viewName + "' wasn't created. In the DB there's an object with the same name. Try another name.", HttpStatusCode.BadRequest);
+            }
+
+            List<string> listColumnNames = columnNames.Split(new char[] { ',' }).ToList();
+
+            //build the SELECT statement
+            string sqlSelect = "SELECT ";
+            for (int i = 0; i < listColumnNames.Count; i++)
+            {
+                if (i > 0)
+                {
+                    sqlSelect += ",";
+                }
+
+                sqlSelect += listColumnNames[i];
+            }
+
+            sqlSelect += " FROM " + tableName;
+
+            if (!string.IsNullOrEmpty(whereClause))
+            {
+                sqlSelect += " WHERE ";
+
+                List<string> listWhereClauses = whereClause.Split(new char[] { ',' }).ToList();
+
+                string columnFilter = string.Empty;
+                string valueFilter = string.Empty;
+
+                for (int i = 0; i < listWhereClauses.Count; i++)
+                {
+                    columnFilter = listWhereClauses[i].Split(new char[] { '=' })[0];
+                    valueFilter = listWhereClauses[i].Split(new char[] { '=' })[1];
+
+                    if (i > 0)
+                    {
+                        sqlSelect += " AND ";
+                    }
+
+                    switch (Type.GetTypeCode(getColumnType(connectionString, tableName, columnFilter)))
+                    {
+                        case TypeCode.DateTime:
+                        case TypeCode.Char:
+                        case TypeCode.String:
+                            sqlSelect += columnFilter + " = '" + valueFilter + "'";
+                            break;
+
+                        case TypeCode.Boolean:
+                        case TypeCode.Byte:
+                        case TypeCode.Decimal:
+                        case TypeCode.Double:
+                        case TypeCode.Int16:
+                        case TypeCode.Int32:
+                        case TypeCode.Int64:
+                        case TypeCode.Single:
+                            sqlSelect += columnFilter + " = " + valueFilter;
+                            break;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(orderbyClause))
+            {
+                sqlSelect += " ORDER BY " + orderbyClause;
+            }
+
+            //create view
+            ExecuteNonQuery(connectionString, CommandType.Text, sqlCreateView + sqlSelect, null, null);
+
+            return WebOperationContext.Current.CreateTextResponse("View created correctly",
+                "application/json; charset=utf-8", Encoding.UTF8);
+        }
+        catch (SqlException ex)
+        {
+            throw new WebFaultException<string>(ex.Message, HttpStatusCode.BadRequest);
+        }
+    }
+    #endregion
+
+    #region "SQL Methods"
+    private System.Type getColumnType(string connectionString, string table, string column)
+    {
+        System.Type type = null;
+
+        using (SqlDataReader rdr = ExecuteReader(connectionString, CommandType.Text, "SELECT " + column + " FROM " + table, null, null))
+        {
+            if (rdr.Read())
+            {
+                type = rdr.GetFieldType(0);
+            }
+        }
+
+        return type;
+    }
+
+    private string getConnectionString(string nameConnectionString)
+    {
+        string connectionString = string.Empty;
+
+        foreach (ConnectionStringSettings cs in ConfigurationManager.ConnectionStrings)
+            if (cs.Name.Equals(nameConnectionString))
+            {
+                connectionString = cs.ConnectionString;
+                break;
+            }
+
+        return connectionString;
+    }
+
+    private object ExecuteScalar(string connectionString, CommandType cmdType, string cmdText,
+        SqlParameter[] commandParameters, int? cmd_Timeout)
+    {
+        SqlCommand cmd = new SqlCommand();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            PrepareCommand(cmd, connection, null, cmdType, cmdText, commandParameters, cmd_Timeout);
+            object val = cmd.ExecuteScalar();
+            cmd.Parameters.Clear();
+            return val;
+        }
+    }
+
+    private int ExecuteNonQuery(string connectionString, CommandType cmdType, string cmdText,
+        SqlParameter[] commandParameters, int? cmd_Timeout)
+    {
+        SqlCommand cmd = new SqlCommand();
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters, cmd_Timeout);
+            int val = cmd.ExecuteNonQuery();
+            cmd.Parameters.Clear();
+            return val;
+        }
+    }
+
+    private SqlDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText,
+        SqlParameter[] commandParameters, int? cmd_Timeout)
+    {
+        SqlCommand cmd = new SqlCommand();
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        try
+        {
+            PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameters, cmd_Timeout);
+
+            SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            cmd.Parameters.Clear();
+            return rdr;
+        }
+        catch (Exception ex)
+        {
+            conn.Close();
+            throw ex;
+        }
+    }
+
+    private void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType,
+        string cmdText, SqlParameter[] cmdParms, int? cmd_Timeout)
+    {
+        if (cmd_Timeout != null && cmd_Timeout.HasValue)
+            cmd.CommandTimeout = cmd_Timeout.Value;
+
+        if ((conn.State != ConnectionState.Open)) conn.Open();
+
+        cmd.Connection = conn;
+        cmd.CommandText = cmdText;
+
+        if (((trans != null))) cmd.Transaction = trans;
+
+        cmd.CommandType = cmdType;
+
+        if (((cmdParms != null)))
+        {
+            SqlParameter parm = null;
+            foreach (SqlParameter parm_loopVariable in cmdParms)
+            {
+                parm = parm_loopVariable;
+                cmd.Parameters.Add(parm);
+            }
+        }
+    }
+    #endregion
+
+    #region "Private Methods"
+    private List<object> getListFormResponse(string idParentForm, List<string> listColumnNames, string formResponseId)
     {
         int idParentForm_INT;
         decimal formResponseId_DECIMAL = 0;
         List<object> listReturn = null;
         string nameType = string.Empty;
+        System.Linq.IOrderedQueryable<FormResponse> items = null;
 
         if (!int.TryParse(idParentForm, out idParentForm_INT))
-            RESTException(string.Format("The ID Parent Form submitted is invalid: '{0}'", idParentForm), HttpStatusCode.BadRequest);
+        {
+            throw new WebFaultException<string>(string.Format("The ID Parent Form submitted is invalid: '{0}'", idParentForm), HttpStatusCode.BadRequest);
+        }
 
         if (!string.IsNullOrEmpty(formResponseId) && !decimal.TryParse(formResponseId, out formResponseId_DECIMAL))
-            RESTException(string.Format("The ID Form Response submitted is invalid: '{0}'", formResponseId), HttpStatusCode.BadRequest);
+        {
+            throw new WebFaultException<string>(string.Format("The ID Form Response submitted is invalid: '{0}'", formResponseId), HttpStatusCode.BadRequest);
+        }
 
         nameType = Form.getFormName(idParentForm_INT);
 
         if (string.IsNullOrEmpty(nameType))
-            RESTException(string.Format("No form with ID = '{0}'", idParentForm), HttpStatusCode.NotFound);
+        {
+            throw new WebFaultException<string>(string.Format("No form with ID = '{0}'", idParentForm), HttpStatusCode.NotFound);
+        }
 
         nameType = nameType.Substring(0, 1).ToUpper() + nameType.Substring(1);
 
@@ -74,8 +285,6 @@ public class Service : IService
 
         using (GRASPEntities db = new GRASPEntities())
         {
-            System.Linq.IOrderedQueryable<FormResponse> items = null;
-
             if (!string.IsNullOrEmpty(formResponseId))
             {
                 items = from rv in db.FormResponse
@@ -100,6 +309,7 @@ public class Service : IService
                                 where r.RVRepeatCount == 0                 //Exclude repeatables vals
                                     && r.type != "SERVERSIDE-CALCULATED"   //Exclude server-side calculated fields
                                     && r.FormResponseID == fr.id
+                                    && listColumnNames.Contains(r.name)
                                 select new { r.id, r.name, r.type, r.value, r.nvalue };
 
                 AddFieldProperty(tb, "ID", typeof(int));
@@ -141,20 +351,40 @@ public class Service : IService
 
                 listReturn = new List<object>();
 
+                if (!string.IsNullOrEmpty(formResponseId))
+                {
+                    responses = from r in db.FormFieldResponses
+                                where r.RVRepeatCount == 0                 //Exclude repeatables vals
+                                    && r.type != "SERVERSIDE-CALCULATED"   //Exclude server-side calculated fields
+                                    && r.FormResponseID == formResponseId_DECIMAL
+                                    && listColumnNames.Contains(r.name)
+                                select new { r.id, r.name, r.type, r.value, r.nvalue };
+                }
+                else
+                {
+                    responses = from r in db.FormFieldResponses
+                                where r.RVRepeatCount == 0                 //Exclude repeatables vals
+                                    && r.type != "SERVERSIDE-CALCULATED"   //Exclude server-side calculated fields
+                                    && r.parentForm_id == idParentForm_INT
+                                    && listColumnNames.Contains(r.name)
+                                select new { r.id, r.name, r.type, r.value, r.nvalue };
+                }
+
                 foreach (FormResponse response in listFormResponse)
                 {
                     object objectFormResponse = Activator.CreateInstance(t);
 
-                    responses = from r in db.FormFieldResponses
-                                where r.RVRepeatCount == 0                 //Exclude repeatables vals
-                                    && r.type != "SERVERSIDE-CALCULATED"   //Exclude server-side calculated fields
-                                    && r.FormResponseID == fr.id
-                                select new { r.id, r.name, r.type, r.value, r.nvalue };
+                    //responses = from r in db.FormFieldResponses
+                    //            where r.RVRepeatCount == 0                 //Exclude repeatables vals
+                    //                && r.type != "SERVERSIDE-CALCULATED"   //Exclude server-side calculated fields
+                    //                && r.FormResponseID == response.id
+                    //                && listColumnNames.Contains(r.name)
+                    //            select new { r.id, r.name, r.type, r.value, r.nvalue };
 
                     string nameProperty = "ID";
                     PropertyInfo pi = t.GetProperty(nameProperty);
 
-                    pi.SetValue(objectFormResponse, int.Parse(fr.id.ToString()), null);
+                    pi.SetValue(objectFormResponse, int.Parse(response.id.ToString()), null);
 
                     foreach (var r in responses)
                     {
@@ -193,27 +423,6 @@ public class Service : IService
         }
 
         return listReturn;
-    }
-
-    Message IService.Surveys(string formId, string formResponseId)
-    {
-        List<object> listReturn = null;
-
-        if (string.IsNullOrEmpty(formId))
-            listReturn = Surveys();
-        else
-            listReturn = getListFormResponse(formId, formResponseId);
-
-        if (listReturn == null || listReturn.Count == 0)
-            throw new WebFaultException<string>("No data", HttpStatusCode.NotFound);
-
-        return WebOperationContext.Current.CreateTextResponse(JsonConvert.SerializeObject(listReturn),
-            "application/json; charset=utf-8", Encoding.UTF8);
-    }
-
-    private void RESTException(string errorMessage, System.Net.HttpStatusCode httpStatusCode)
-    {
-        throw new WebFaultException<string>(errorMessage, httpStatusCode);
     }
 
     private static void AddFieldProperty(TypeBuilder tb, string nameField, Type type)
@@ -258,7 +467,7 @@ public class Service : IService
 
         CustomAttributeBuilder attr = new CustomAttributeBuilder(attrType.GetConstructor(Type.EmptyTypes), new object[] { });
 
-        PropertyBuilder pb = tb.DefineProperty(nameProperty, PropertyAttributes.HasDefault, type, null);
+        PropertyBuilder pb = tb.DefineProperty(nameProperty, System.Reflection.PropertyAttributes.HasDefault, type, null);
 
         pb.SetCustomAttribute(attr);
 
@@ -270,4 +479,5 @@ public class Service : IService
         FieldBuilder fb = tb.DefineField(nameField, type, FieldAttributes.Private);
         return fb;
     }
+    #endregion
 }
