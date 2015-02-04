cc68094e8f680b2b9ade78e697af6ef01c88136e
unknown <Sa3d@WFPPSJER0127572.global.wfp.org>
2015-02-04 14:46:54 +0200

    Add FormResponseServerStatus.cs file

---
+++ src/App_Code/FormResponseServerStatus.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FormResponseServerStatus
+/// </summary>
+public partial class FormResponseServerStatus
+{
+    public FormResponseServerStatus()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static bool IsFormSaved(string fileName)
+    {
+        bool isSaved = false;
+        try
+        {
+            using (GRASPEntities db = new GRASPEntities())
+            {
+                FormResponseServerStatus formResponseServerStatus = (from fr in db.FormResponseServerStatus
+                                                                     where fr.InstanceUniqueIdentifier == fileName
+                                                                     select fr).FirstOrDefault();
+                if (formResponseServerStatus != null)
+                {
+                    isSaved = formResponseServerStatus.IsSavedToServer;
+                }
+            }
+        }
+        catch (Exception)
+        {
+        }
+
+        return isSaved;
+    }
+
+    public static bool InsertOrUpdateStatus(string fileName, bool isSaved)
+    {
+        bool isSuccess = false;
+
+        try
+        {
+            using (GRASPEntities db = new GRASPEntities())
+            {
+                var formResponseServerStatus = (from fr in db.FormResponseServerStatus
+                                                                     where fr.InstanceUniqueIdentifier == fileName
+                                                                     select fr).FirstOrDefault();
+                if (formResponseServerStatus != null)
+                {
+                    formResponseServerStatus.IsSavedToServer = isSaved;
+                }
+                else
+                {
+                    formResponseServerStatus = new FormResponseServerStatus();
+                    formResponseServerStatus.InstanceUniqueIdentifier = fileName;
+                    formResponseServerStatus.IsSavedToServer = isSaved;
+                    db.FormResponseServerStatus.Add(formResponseServerStatus);
+                }
+
+                db.SaveChanges();
+                isSuccess = true;
+            }
+        }
+        catch (Exception)
+        {
+        }
+
+        return isSuccess;
+    }
+
+}
