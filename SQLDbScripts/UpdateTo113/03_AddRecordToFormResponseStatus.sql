cac4979aca0226ad857e9b18b7dbf1e12bfe68a9
geommm <george@muammar.net>
2016-02-01 16:15:08 +0200

    Delete all files

--- SQLDbScripts/UpdateTo113/03_AddRecordToFormResponseStatus.sql
+++
@@ -1,5 +0,0 @@
-
-    INSERT INTO GRASP.dbo.FormResponseStatus (ResponseStatusName,ResponseStatusIndex,ResponseStatusDependency) 
-  VALUES ('Deleted',4,0);
-    INSERT INTO GRASP.dbo.FormResponseStatus (ResponseStatusName,ResponseStatusIndex,ResponseStatusDependency) 
-  VALUES ('Pending',2,1);
--- src/_uc/UserEdit.ascx
+++
@@ -1,3 +0,0 @@
-﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="UserEdit.ascx.cs" Inherits="_uc_UserEdit" %>
-
-
